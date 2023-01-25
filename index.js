6fe9f1df414524fc346f7eacaa28ce6ab7390b50
Austin Ozor <augustineozor@gmail.com>
2023-01-26 00:35:46 +0300

    updated

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const express = require("express");
-const path =require('path')
+const path = require('path');
+const server = express('server');
 
-const server = express();
 
+//middleware definition
+server.use(express.static(path.join(__dirname, 'public')))
 
-//middlewares
-
-const serveHomePage = (req, res) => {
-    //res.send("Welcome to the home page")
-    //1.find the html file and send it to the client
-    const homePageFilePath = path.join(__dirname, 'public', 'index.html');
-    res.sendFile(homePageFilePath);
-}
-
-const serverServicesPage = (req, res) => {
-    const servicesPageFilePath = path.join(__dirname, 'public', 'services.html');
-    res.sendFile(servicesPageFilePath );
-}
+// const serveHomePage = (req, res) => {
+//     //res.send("Welcome to the home page")
+//     //1.find the html file and send it to the client
+//     const homePageFilePath = path.join(__dirname, 'public', 'index.html');
+//     res.sendFile(homePageFilePath);
+// }
+// const serverServicesPage = (req, res) => {
+//     const servicesPageFilePath = path.join(__dirname, 'public', 'services.html');
+//     res.sendFile(servicesPageFilePath );
+// }
 //routes definitions
-server.get('/', serveHomePage);
-server.get('/services', serverServicesPage);
+// server.get('/', serveHomePage);
+// server.get('/services', serverServicesPage);
 
 server.listen(3003, () => console.log("Server is ready for use"));
